c7cb1b061c82859823cd8d2c5269c935cec2bef6
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2017-07-04 17:18:11 +0200

    multiple games simultantesosnly

--- server.js
+++ server.js
@@ -4,17 +4,72 @@ const wss = new WebSocket.Server({ port: 8080 });
 const shortid = require("shortid");
 const vector = require("./vector");
 const player = require("./player");
-const Game = require("./game");
 
 const config = require("./config.json");
 
-let gamesession;
+let inQueue = new Set();
+
+let gamesessions = new Set();
+
+function broadcast(clients, message) {
+	let newmsg = Object.assign({}, message);
+	delete newmsg.clients;
+	let msg = JSON.stringify(newmsg);
+	setTimeout(function () {
+		clients.forEach(cl => {
+			try {
+			cl.send(msg);
+			} catch(err) {
+				clients.splice(clients.indexOf(cl), 1);
+			}
+		})
+	}, latency)
+}
+
+let maps = {
+	1: {
+		width: 100,
+		height: 100
+	},
+	2: {
+		width: 200,
+		height: 300
+	}
+}
+
+class Game {
+	constructor({ clients, players, id, map = maps[1] }) {
+		this.players = players;
+		this.map = map;
+		this.id = id;
+		this.clients = clients;
+	}
+	updateGameState() {
+		broadcast(this.clients, { type: "gamestate", data: this.getInfo() });
+		setTimeout(this.updateGameState.bind(this), 30);
+	}
+	start() {
+		this.updateGameState();
+	}
+	update() {
+		this.players.forEach(pl => {
+			pl.pos.add(pl.vel);
+		})
+	}
+	getInfo() {
+		return { players: this.players, map: this.map };
+	}
+}
 
 function initGame(clients) {
 	let players = clients.map(pl => {
 		return new player({ pos: new vector(0, 0), id: pl.id });
 	});
-	return new Game({ players: players, id: shortid.generate() });
+	let game = new Game({ clients, players: players, id: shortid.generate() });
+	clients.forEach(cl => {
+		cl.game = game;
+	})
+	return game;
 }
 
 function findObjectWithPropertyInArray(array, key, value) {
@@ -28,27 +83,17 @@ if (config.dev && config.latency) {
 	latency = config.latency;
 }
 
-function broadcast(server, message) {
-	setTimeout(function () {
-		server.clients.forEach(cl => {
-			cl.send(JSON.stringify(message));
-		})
-	}, latency)
-}
-
-function updateGameState() {
-	broadcast(wss, { type: "gamestate", data: gamesession });
-	setTimeout(updateGameState, 30);
-}
-
 wss.on("connection", function (ws) {
 	ws.id = shortid.generate();
 	ws.send(JSON.stringify({ type: "id", data: ws.id }));
+	inQueue.add(ws);
 
-	if (wss.clients.size >= 2) {
-		gamesession = initGame(Array.from(wss.clients));
-		broadcast(wss, { type: "initgame", data: gamesession });
-		updateGameState();
+	if (inQueue.size >= 2) {
+		let game = initGame(Array.from(inQueue));
+		gamesessions.add(game);
+		inQueue = new Set();
+		broadcast(game.clients, { type: "initgame", data: game.getInfo() });
+		game.start();
 	}
 
 	ws.on("message", function (message) {
@@ -56,7 +101,7 @@ wss.on("connection", function (ws) {
 			let data = JSON.parse(message);
 			switch (data.type) {
 				case "playerupdate":
-					let pl = findObjectWithPropertyInArray(gamesession.players, "id", data.data.id);
+					let pl = findObjectWithPropertyInArray(ws.game.players, "id", data.data.id);
 					if (!pl) return;
 					pl.pos = data.data.pos;
 					pl.vel = data.data.vel;
